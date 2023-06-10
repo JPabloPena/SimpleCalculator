3c191b713d23534440c1e5e57acecd9188b9c102
Juan Pablo Peña F <pablo27-99@hotmail.com>
2023-06-09 23:28:25 -0500

    Sum and subtract working correctly

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const $ = (selector) => document.querySelector(selector)
 
 const result = $('#result')
+const resultPreview = $('#result-preview')
 /* Clear and Delete */
 const buttonClear = $('.button-clear')
 const buttonDelete = $('.button-delete')
@@ -18,7 +19,7 @@ const button8 = $('.num-8').addEventListener('click', () => {calculate('8')})
 const button9 = $('.num-9').addEventListener('click', () => {calculate('9')})
 /* ---- Operators ----*/
 const buttonDiv = $('.div').addEventListener('click', () => {calculate('/')})
-const buttonMul = $('.mul').addEventListener('click', () => {calculate('*')})
+const buttonMul = $('.mul').addEventListener('click', () => {calculate('x')})
 const buttonMinus = $('.minus').addEventListener('click', () => {calculate('-')})
 const buttonSum = $('.sum').addEventListener('click', () => {calculate('+')})
 const buttonEqual = $('.equal').addEventListener('click', () => {calculate('=')})
@@ -27,28 +28,55 @@ let aux = ''
 let firstNumberAux = 0
 let operatorAux = ''
 let finalResult = 0
+let isNextANumber = false
+let wasAEqualBefore = false
+let minusCounterAux = 0
+
 function calculate(num) {
-    console.log('Num: '+num)
-    if (num === '/' || num === '*' || num === '-' || num === '+') {
-        operatorAux = num
-        firstNumberAux = parseFloat(aux)
-        aux = ''
-        // result.innerText = ''
-        console.log({operatorAux, firstNumberAux, aux})
-    } else if (num === '=') {
+    const operatorsConditional = num === '/' || num === 'x' || num === '-' || num === '+' || num === '='
+    if (operatorsConditional && isNextANumber === false) {
+        if (wasAEqualBefore === true) resultPreview.innerText = finalResult
+        if (num !== '=') operatorAux = num // -
+        console.log({finalResult})
+        resultPreview.innerText += aux + num
         switch (operatorAux) {
+            case '-':
+                console.log('Estoy restando')
+                if (minusCounterAux === 0) {
+                    aux === '' ? finalResult += 0 : finalResult += parseFloat(aux) // 10 + 0 = 10
+                    minusCounterAux++ // 1
+                    break
+                }
+                aux === '' ? finalResult += 0 : finalResult += -parseFloat(aux)
+                console.log('Resta ' + finalResult)
+                result.innerText = finalResult // 10
+                minusCounterAux = 0
+                break
             case '+':
                 console.log('Estoy sumando')
-                finalResult += firstNumberAux + parseFloat(aux)
-                aux = '0'
-                result.innerText = finalResult
+                aux === '' ? finalResult += 0 : finalResult += parseFloat(aux) // 10 + 0 = 10
+                console.log('Suma ' + finalResult)
+                result.innerText = finalResult // 10
                 break
         }
-    } else {
+        aux = ''
+        isNextANumber = true
+        wasAEqualBefore = false
+        if (num === '=') {
+            isNextANumber = false
+            wasAEqualBefore = true
+        }
+    } else if (!operatorsConditional) {
         if (aux.length === 0) {
             result.innerText = ''
         }
-        aux += num
-        result.innerText += num
+        if (wasAEqualBefore === true) {
+            finalResult = 0
+            wasAEqualBefore = false
+            resultPreview.innerText = ''
+        }
+        aux += num // 10
+        result.innerText += num // 10
+        isNextANumber = false
     }
 }
