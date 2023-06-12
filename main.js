225bb23d629a13f1498192a69f7f5d5eac6ac08e
Juan Pablo Peña F <pablo27-99@hotmail.com>
2023-06-12 12:36:55 -0500

    Delete console logs

--- main.js
+++ main.js
@@ -38,8 +38,6 @@ let finalResult = 0
 let isEqualActivated = false
 let isFirstOperation = true
 function operation(operator, number) {
-    console.log({operator, number})
-
     // Is it's the first operation add 0 to avoid show " /" or " +", etc.
     if (number === '' && arrNumbers.length === 0) number = '0'
 
@@ -48,8 +46,6 @@ function operation(operator, number) {
         arrNumbers[arrNumbers.length - 1] = operator
         const newPreviewMessage = resultPreview.innerText.slice(0, -2)
         if (newPreviewMessage[0] !== '(' || newPreviewMessage[newPreviewMessage.length -1 ] !== ')') {
-            console.log({newPreviewMessage})
-            console.log('True? ', /[+\-*\/]/.test(newPreviewMessage))
             if (/[+\-*\/]/.test(newPreviewMessage)) {
                 resultPreview.innerText = `(${newPreviewMessage})`
             } else {
@@ -76,68 +72,51 @@ function operation(operator, number) {
         const firstNumber = arrNumbers.shift()
         arrNumbers.shift()
         arrNumbers.unshift(firstNumber)
-        console.log(`I'm the new array ${arrNumbers}`)
     }
 
     // When you made an operation, but you want continue operating with the result
     if (typeof(arrNumbers[1]) === 'number') {
         arrNumbers.shift()
         resultPreview.innerText = `${number} ${operator}`
-        console.log(`I'm the second new array ${arrNumbers}`)
     }
 
-    console.log({arrNumbers})
     auxNum = ''
 
     if (arrNumbers[arrNumbers.length - 1] !== operator && arrNumbers.length !== 0) {
-        console.log(arrNumbers.length)
         arrNumbers.push(operator)
     }
 
     if (arrNumbers.length === 4) {
-        console.log('Tengo 4')
         const auxOperator = arrNumbers[arrNumbers.length - 1]
-        console.log({auxOperator})
         switch (auxOperator) {
             case '+':
-                console.log(`I'm sum`)
                 finalResult = eval(arrNumbers[0], arrNumbers[2], arrNumbers[1])
                 result.innerText = finalResult
                 arrNumbers = [finalResult, '+']
-                console.log({arrNumbers})
                 break
             case '-':
-                console.log(`I'm sub`)
                 finalResult = eval(arrNumbers[0], arrNumbers[2], arrNumbers[1])
                 result.innerText = finalResult
                 arrNumbers = [finalResult, '-']
-                console.log({arrNumbers})
                 break
             case 'x':
                 if (arrNumbers[1] !== '+' && arrNumbers[1] !== '-') {
-                    console.log(arrNumbers[1])
-                    console.log(`I'm mul`)
                     finalResult = eval(arrNumbers[0], arrNumbers[2], arrNumbers[1])
                     result.innerText = finalResult
                     arrNumbers = [finalResult, 'x']
-                    console.log({arrNumbers})
                     break
                 }
                 break
             case '/':
                 if (arrNumbers[1] !== '+' && arrNumbers[1] !== '-') {
-                    console.log(arrNumbers[1])
-                    console.log(`I'm div`)
                     finalResult = eval(arrNumbers[0], arrNumbers[2], arrNumbers[1])
                     result.innerText = finalResult
                     arrNumbers = [finalResult, '/']
-                    console.log({arrNumbers})
                     break
                 }
                 break
             case '=':
                 if (isEqualActivated === false) {
-                    console.log(`I'm the result`)
                     if (arrNumbers[1] === '=' && arrNumbers[3] === '=') {
                         arrNumbers = [arrNumbers[0]]
                         break
@@ -146,7 +125,6 @@ function operation(operator, number) {
                         arrNumbers = [finalResult]
                     }
                     result.innerText = finalResult
-                    console.log({arrNumbers})
                     isEqualActivated = true
                     break
                 }
@@ -154,32 +132,24 @@ function operation(operator, number) {
                 break
         }
     } else if (arrNumbers.length === 6) {
-        console.log('Tengo 6')
         const auxOperator = arrNumbers[arrNumbers.length - 3]
         switch (auxOperator) {
             case 'x':
-                console.log(`I'm mul`)
                 let auxMul = 0
                 auxMul = arrNumbers[2] * arrNumbers[4]
                 finalResult = eval(arrNumbers[0], auxMul, arrNumbers[1])
                 result.innerText = finalResult
-                // arrNumbers = [finalResult, arrNumbers[1]]
                 arrNumbers = [finalResult]
-                console.log({arrNumbers})
                 break
             case '/':
-                console.log(`I'm div`)
                 let auxDiv = 0
                 auxDiv = arrNumbers[2] / arrNumbers[4]
                 finalResult = eval(arrNumbers[0], auxDiv, arrNumbers[1])
                 result.innerText = finalResult
-                // arrNumbers = [finalResult, arrNumbers[1]]
                 arrNumbers = [finalResult]
-                console.log({arrNumbers})
                 break
         }
     }
-    console.log(arrNumbers.length)
     isFirstOperation = false
 }
 
